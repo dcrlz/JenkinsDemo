50be25eb8b935b1f8476dba6c93603c74f503e0f
Daniel <dansartori1@gmail.com>
2015-05-06 10:01:01 -0300

    initial commit

---
+++ JenkinsDemo/JenkinsDemo.API.Tests/ValuesControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using JenkinsDemo.API.Controllers;
+
+namespace JenkinsDemo.API.Tests
+{
+    [TestFixture]
+    public class ValuesControllerTests
+    {
+        [Test]
+        public void Get_WithoutParameters_ReturnsArrayOfStringWithTwoValues()
+        {
+            var controller = new ValuesController();
+
+            var result = controller.Get();
+
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("value1", result.FirstOrDefault());
+            Assert.AreEqual("value2", result.LastOrDefault());
+        }
+
+        [Test]
+        public void Get_PassingId_ReturnsString()
+        {
+            var controller = new ValuesController();
+
+            var result = controller.Get(1);
+
+            Assert.AreEqual("value", result);
+        }
+    }
+}
---
+++ JenkinsDemo/JenkinsDemo.API/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JenkinsDemo.API")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JenkinsDemo.API")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fef141de-7efb-425e-9365-e225e6745d3d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
